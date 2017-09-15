1d3ae1b2ca44d07b712d1f28d36d1109f92f19d1
WorldPierce <bildo316@gmail.com>
2017-09-14 22:07:29 -0400

    Unity Rolling ball game project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Late update runs every frame and is garunteed to update
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+    private int count;
+    public Text countTxt;
+    public Text winTxt;
+
+    private void Start()
+    {
+        count = 0;
+        rb = GetComponent<Rigidbody>();
+        setCountTxt();
+        winTxt.text = "";
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Destroy(other.gameObject);
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            setCountTxt();
+            if(count == 12)
+            {
+                winTxt.text = "You Win!";
+            }
+        }
+    }
+
+    void setCountTxt()
+    {
+        countTxt.text = "Count " + count.ToString();
+    }
+}
